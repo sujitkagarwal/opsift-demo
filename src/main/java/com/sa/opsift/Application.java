e71547d7bddb87922ca49e1d16f41400f3d8b669
sujitagarwal <sujit21agarwal@gmail.com>
2018-10-15 10:43:18 +0200

    first commit

---
+++ src/main/java/com/sa/opsift/Application.java
@@ -0,0 +1,24 @@
+package com.sa.opsift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
+@RestController
+class TestController{
+
+
+	@GetMapping("/hello")
+	String getHello(){
+		return "Hello";
+	}
+}
